c161311d51afaea2adae54d4f29ad1924e3adef5
xiaobei-ihmhny <legend0508@163.com>
2018-02-02 07:45:53 +0800

    临时提交

---
+++ microserver-demo/microserver-demo-api/src/main/java/com/ihmhny/demo/api/service/UserService.java
@@ -0,0 +1,12 @@
+package com.ihmhny.demo.api.service;
+
+/**
+ * @author: xiaobei
+ * @createTime: 2018-02-02 7:44
+ * @since: JDK 1.8
+ * @description:
+ */
+public interface UserService {
+
+
+}
---
+++ microserver-demo/microserver-demo-web/src/main/java/com/ihmhny/demo/web/controller/RedisController.java
@@ -0,0 +1,12 @@
+package com.ihmhny.demo.web.controller;
+
+/**
+ * @author: xiaobei
+ * @createTime: 2018-02-02 7:35
+ * @since: JDK 1.8
+ * @description: 测试Redis服务
+ */
+public class RedisController {
+
+
+}
